2bbb68386bfffac12380bbe0838d46a53b4677e9
Serena Tay <tays@oregonstate.edu>
2019-05-14 17:34:45 -0700

    Update

--- Database/data_definitions.sql
+++ Database/data_definitions.sql
@@ -1,29 +1,12 @@
--- MySQL dump 10.13  Distrib 5.1.66, for redhat-linux-gnu (x86_64)
---
--- Host: mysql.eecs.oregonstate.edu    Database: CS275
--- ------------------------------------------------------
--- Server version	5.1.65-community-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
 --
 -- Table structure for table `club`
 --
-DROP DATABASE IF EXISTS `club`
-CREATE DATABASE `club`(
+DROP TABLE IF EXISTS `club`;
+CREATE TABLE `club`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(100) NOT NULL,
     PRIMARY KEY (`id`)
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `club`
@@ -31,7 +14,7 @@ CREATE DATABASE `club`(
 
 LOCK TABLES `club` WRITE;
 /*!40000 ALTER TABLE `club` DISABLE KEYS */;
-INSERT INTO `club` VALUES (1,'Arsenal'),(2,'Malmo FC'),(3,'Athletico Madrid'),(4,'Roma'),(5,'PSG');
+INSERT INTO `club` VALUES (1,'Arsenal'),(2,'Malmo FC'),(3,'Athletico Madrid'),(4,'Roma'),(5,'Paris Saint Germain');
 /*!40000 ALTER TABLE `club` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -41,16 +24,12 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `cashflow`;
 CREATE TABLE `cashflow`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `club_id` int(11) NOT NULL,
+    `club_id` int(11),
     `year` smallint(2) NOT NULL,
     `amount` int(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
-    CONSTRAINT cashflow_club
-    FOREIGN KEY (club_id) REFERENCES club (id) 
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    CONSTRAINT `cashflow_club` FOREIGN KEY (`club_id`) REFERENCES `club` (`id`)  ON DELETE CASCADE ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `cashflow`
@@ -69,20 +48,16 @@ UNLOCK TABLES;
 DROP TABLE IF EXISTS `player`;
 CREATE TABLE `player`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
-    `club_id` int(11) NOT NULL,
+    `club_id` int(11) DEFAULT NULL,
     `first_name` varchar(100) NOT NULL,
     `last_name` varchar(100) NOT NULL,
     `country_id` int(11) NOT NULL,
     `fee` int(11) NOT NULL DEFAULT 0,
     `market_value` int(11) NOT NULL DEFAULT 0,
     PRIMARY KEY (`id`),
-    CONSTRAINT player_club
-    FOREIGN KEY (club_id) REFERENCES club (id) 
-    CONSTRAINT player_country
-    FOREIGN KEY (country_id) REFERENCES country (id) 
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
+    CONSTRAINT `player_club` FOREIGN KEY (`club_id`) REFERENCES `club` (`id`) ON DELETE SET NULL ON UPDATE CASCADE, 
+    CONSTRAINT `player_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`)
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 
@@ -107,11 +82,8 @@ DROP TABLE IF EXISTS `country`;
 CREATE TABLE `country`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    PRIMARY KEY (`id`), 
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    PRIMARY KEY (`id`) 
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `country`
@@ -132,20 +104,18 @@ DROP TABLE IF EXISTS `league`;
 CREATE TABLE `league`(
     `id` int(11) NOT NULL AUTO_INCREMENT,
     `name` varchar(255) NOT NULL,
-    `country_id` varchar(255) NOT NULL,
+    `country_id` int(11) DEFAULT NULL,
     PRIMARY KEY (`id`), 
-    FOREIGN KEY (country_id) REFERENCES country (id) 
-    ON DELETE SET NULL
-    ON UPDATE CASCADE
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+    CONSTRAINT `league_country` FOREIGN KEY (`country_id`) REFERENCES `country` (`id`)  ON DELETE SET NULL ON UPDATE CASCADE
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 --
 -- Dumping data for table `league`
 --
 LOCK TABLES `league` WRITE;
 /*!40000 ALTER TABLE `league` DISABLE KEYS */;
-INSERT INTO `league` VALUES (1,"English Premier League",1),(2,"Championship",1),(3,"Allsvenskan",2),(4,"La Liga", 3),(5,"Seria A"), (6, "Ligue 1",5);
+INSERT INTO `league` VALUES (1,"English Premier League",1),(2,"Championship",1),(3,"Allsvenskan",2),(4,"La Liga", 3),(5,"Seria A", 4), (6, "Ligue 1",5);
 /*!40000 ALTER TABLE `league` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -154,33 +124,20 @@ UNLOCK TABLES;
 --
 DROP TABLE IF EXISTS `club_league`;
 CREATE TABLE `club_league`(
-    league_id INTEGER NOT NULL,
-    club_id INTEGER NOT NULL,
+    `league_id` INTEGER NOT NULL,
+    `club_id` INTEGER NOT NULL,
     `year` int(11) NOT NULL,
-    FOREIGN KEY (league_id) REFERENCES league (league_id) ON DELETE RESTRICT ON UPDATE CASCADE,
-    FOREIGN KEY (club_id) REFERENCES club (club_id) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (`league_id`) REFERENCES league (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (`club_id`) REFERENCES club (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
     PRIMARY KEY (league_id, club_id)
-)ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
+)ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 
 --
 -- Dumping data for table `club_league`
 --
 LOCK TABLES `club_league` WRITE;
 /*!40000 ALTER TABLE `club_league` DISABLE KEYS */;
-INSERT INTO `club_league` VALUES (1,"English Premier League",1),(2,"Championship",1),(3,"Allsvenskan",2),(4,"La Liga", 3),(5,"Seria A"), (6, "Ligue 1",5);
+INSERT INTO `club_league` VALUES (1,1,2000),(2,1,1999),(3,2,2003),(4,3, 2006),(5,4, 1988), (6,5, 1989);
 /*!40000 ALTER TABLE `club_league` ENABLE KEYS */;
 UNLOCK TABLES;
-
-
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2013-02-04 12:54:40
--- Database/data_manipulation_queries.sql
+++ Database/data_manipulation_queries.sql
@@ -2,46 +2,67 @@
 
 -- get all club IDs and names
 SELECT club.id, club.name
-FROM club
+FROM club;
 
 -- get all club IDs and names and their respective income and expenditures for all years
 SELECT *
 FROM club
+INNER JOIN cashflow ON club.id = cashflow.club_id
+ORDER BY cashflow.year, cashflow.amount DESC;
+
+-- get income and expenditures of a specific club for all years
+SELECT *
+FROM club
 INNER JOIN cashflow ON club.id =cashflow.club_id
-ORDER BY cashflow.year, cashflow.amount DESC
+WHERE club.id = :club_id_input
+ORDER BY cashflow.year, cashflow.amount DESC;
 
 -- get all players and their current information
-SELECT *
+SELECT player.first_name, player.last_name, player.fee, player.market_value, country.name, club.name
 FROM player 
 INNER JOIN country ON player.country_id = country.id
-INNER JOIN country ON player.club_id = club.id
+INNER JOIN club ON player.club_id = club.id;
+
+-- get a specific player's current information
+SELECT player.first_name, player.last_name, player.fee, player.market_value, country.name, club.name
+FROM player 
+INNER JOIN country ON player.country_id = country.id
+INNER JOIN club ON player.club_id = club.id
+WHERE player.id = :player_id_input
+
+-- get all leagues that a club has been part of
+SELECT league.name, club_league.year FROM club
+INNER JOIN club_league ON club.id = club_league.club_id 
+INNER JOIN league ON league.id = club_league.league_id
+WHERE club.id = :club_id_input
 
 -- add a new player
 INSERT INTO player (club_id, first_name, last_name, country_id, fee, market_value)
-VALUES (:club_id_input, :first_name_input, :last_name_input, :country_id_input, :fee_input, :market_value_input)
+VALUES (:club_id_input, :first_name_input, :last_name_input, :country_id_input, :fee_input, :market_value_input);
 
 -- add a new club
 INSERT INTO club (`name`) 
-VALUES (:name_input)
+VALUES (:name_input);
+
+-- add a new country
+INSERT INTO country (`name`)
+VALUES (:country_name_input);
 
 -- add a new cashflow record
 INSERT INTO cashflow (club_id, year, amount) 
-VALUES (:club_id_input, :year_input, :amount_input) 
+VALUES (:club_id_input, :year_input, :amount_input);
 
 -- associate a club with a league (M-to-M relationship addition)
-INSERT INTO club_league (league_id, club_id, year) VALUES (:league_id_input, :club_id_input, :year_input) 
+INSERT INTO club_league (league_id, club_id, year) VALUES (:league_id_input, :club_id_input, :year_input);
 
 -- update a player's information
-UPDATE player SET first_name = :first_name_input, last_name = :last_name_input, country = :country_id_input, fee = :fee_input, market_value = :market_value_input WHERE id = :player_id_input
+UPDATE player SET first_name = :first_name_input, last_name = :last_name_input, country_id = :country_id_input, fee = :fee_input, market_value = :market_value_input WHERE id = :player_id_input;
 
 -- delete a player
-DELETE FROM player WHERE id = :player_id_input
+DELETE FROM player WHERE id = :player_id_input;
 
 -- update a club's information
-UPDATE club SET `name` = :name_input, country = :country_id_input WHERE id = :club_id_input
+UPDATE club SET `name` = :name_input WHERE club.id = :club_id_input;
 
--- delete a club
-DELETE FROM club WHERE id = :club_id_input
-
--- dis-associate a certificate from a person (M-to-M relationship deletion)
-DELETE FROM club_league WHERE league_id = :league_id_input AND club_id = :club_id_input AND year = :year_input
+-- dis-associate a club from a league (M-to-M relationship deletion)
+DELETE FROM club_league WHERE club_league.league_id = :club_league_league_id_input AND club_league.club_id = :club_league_club_id_input;
