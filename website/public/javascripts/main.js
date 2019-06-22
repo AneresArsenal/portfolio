d3bbfc57f68d558a9ddcd1c4d481d264734e6fdb
Serena Tay <tays@oregonstate.edu>
2019-06-21 23:30:39 -0700

    edits

--- website/public/javascripts/main.js
+++ website/public/javascripts/main.js
@@ -16,3 +16,19 @@ $('.progress-fill').each(function(){
   var percent = $(this).html();
   $(this).parent().css('width', percent);
 });
+
+var slideIndex = 0;
+showTravelSlides();
+
+
+function showTravelSlides() {
+    let i;
+    var slides = document.getElementsByClassName("mySlides");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1 }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(showTravelSlides, 5000); // Change image every 5 seconds
+}
