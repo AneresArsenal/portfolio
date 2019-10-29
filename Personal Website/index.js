e0055eca3fd3cce69a8d0b176fb343089fac743b
Serena Tay <tays@oregonstate.edu>
2019-10-29 12:03:24 -0700

    Create backup copy for website hosted on Heroku

--- Personal Website/index.js
+++ Personal Website/index.js
@@ -1,11 +1,25 @@
+// const express = require('express')
+// const path = require('path')
+// const PORT = process.env.PORT || 5000
+
+// express()
+//   .use(express.static(path.join(__dirname, 'public')))
+//   .set('views', path.join(__dirname, 'views'))
+//   .set('view engine', 'ejs')
+//   .get('/', (req, res) => res.render('pages/index'))
+//   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
 const express = require('express')
-const bodyParser = require('body-parser');
 const path = require('path')
+const bodyParser = require('body-parser');
+const app = express();
 const port = process.env.PORT || 5000
 
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
 express()
-  .use(bodyParser.urlencoded({ extended: false }))
-  .use(bodyParser.json())
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'pug')
